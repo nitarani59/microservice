951a83f6e61bba329bc64a7447ac84d300bcc1b2
nitarani59 <raninita59@gmail.com>
2025-01-28 12:41:08 +0530

    Implemented infrastructure

---
+++ borrowingservice/src/main/java/com/springcloud/borrowingservice/BorrowingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.borrowingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorrowingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorrowingserviceApplication.class, args);
+	}
+
+}
